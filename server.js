766cd7126ba89e1aeb0708a2bb3314bd4f34cee2
zakeyah <mom11omar@gmail.com>
2021-05-02 18:45:16 +0300

    add .github

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ const app = express();
 
 // add routes
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Server home route');
+  res.status(200).send('Hello from Server home route');
 });
 
 app.get('/info', (req, res) => {
-    res.status(200).json({
-        name: 'Mohammad',
-        age: 29
-    });
+  res.status(200).json({
+    name: 'Mohammad',
+    age: 29
+  });
 });
 
 app.get('/bad-request', (req, res) => {
-    throw new Error('manual error');
+  throw new Error('manual error');
 });
 
 app.get('/bad-request-2', (req, res) => {
-    let arr;
-    arr.push(2);
+  let arr;
+  arr.push(2);
 });
 
 // Handlers -> Middlewares
@@ -35,10 +35,10 @@ app.use(errorHandler);
 
 
 function start(port) {
-    app.listen(port, () => console.log(`App is Runnning on ${port}`));
+  app.listen(port, () => console.log(`App is Runnning on ${port}`));
 }
 
 module.exports = {
-    app: app,
-    start: start
+  app: app,
+  start: start
 }
